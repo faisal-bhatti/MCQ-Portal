ba0aca5dd9a38b0ebe2f34ccf33d689affb229ef
Im Genius <im.genius.man@gmail.com>
2014-07-11 11:41:07 +0500

    auto generate password

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,5 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require jquery.fancybox.pack
-//= require intro
-//= require knockout-2.2.1
+//= require pwdwidget
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,15 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :first_name, :grade,:group,:contact
-   validates_presence_of :username
+   # validates_presence_of :username
    validates_presence_of :first_name
    validates_presence_of :grade
    validates_presence_of :group
    validates_presence_of :contact
 
+    # validates_each :username do |record, attr, value|
+    #   record.errors.add(attr, 'must start with upper case') if value =~ /\A[a-z]/
+    # end
 
    has_many :quereys
   # attr_accessible :title, :body
